f56a84c30eceae2609b1de955144bbb3abaf6305
Karan Sandhu <123186222+SandhuKaran@users.noreply.github.com>
2023-04-07 15:26:54 -0400

    Update Huffman.c

--- Huffman.c
+++ Huffman.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <math.h>
+
+#define SYMBOL_COUNT 256
+#define FILENAME_MAX_SIZE 256
+#define BUFFER_SIZE 256
 
 typedef struct huff_node {
     char symbol;
@@ -65,3 +68,42 @@ void huff_heap_push(huff_heap_t *heap, huff_node_t *node) {
     }
 }
 
+huff_node_t *huff_heap_pop(huff_heap_t *heap) {
+    huff_node_t *node = heap->nodes[0];
+    heap->nodes[0] = heap->nodes[--heap->count];
+    int i = 0;
+    while (true) {
+        int left_child = i * 2 + 1;
+        int right_child = i * 2 + 2;
+        if (left_child >= heap->count) break;
+        int min_child = left_child;
+        if (right_child < heap->count && heap->nodes[right_child]->frequency < heap->nodes[left_child]->frequency) {
+            min_child = right_child;
+        }
+        if (heap->nodes[i]->frequency <= heap->nodes[min_child]->frequency) break;
+        huff_node_t *tmp = heap->nodes[i];
+        heap->nodes[i] = heap->nodes[min_child];
+        heap->nodes[min_child] = tmp;
+        i = min_child;
+    }
+    return node;
+}
+
+huff_node_t *huff_build_tree(int *frequencies) {
+    huff_heap_t *heap = new_huff_heap();
+    for (int i = 0; i < SYMBOL_COUNT; i++) {
+        if (frequencies[i] > 0) {
+            huff_node_t *node = new_huff_node((char) i, frequencies[i], NULL, NULL);
+            huff_heap_push(heap, node);
+        }
+    }
+    while (heap->count > 1) {
+        huff_node_t *node1 = huff_heap_pop(heap);
+        huff_node_t *node2 = huff_heap_pop(heap);
+        huff_node_t *parent = new_huff_node('\0', node1->frequency + node2->frequency, node1, node2);
+        huff_heap_push(heap, parent);
+    }
+    huff_node_t *root = huff_heap_pop(heap);
+    free(heap);
+    return root;
+}
